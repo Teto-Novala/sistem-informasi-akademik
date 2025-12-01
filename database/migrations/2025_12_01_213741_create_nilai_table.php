60e3bbcf18f359f9622d664f92f2dcaa8dcb9261
Teto-Novala <tetocoding@gmail.com>
2025-12-02 04:42:58 +0700

    feat: create all table

---
+++ database/migrations/2025_12_01_213741_create_nilai_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('nilai', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('mahasiswa_id')->constrained('mahasiswa');
+            $table->foreignId('dosen_id')->constrained('dosen');
+            $table->foreignId('matakuliah_id')->constrained('matakuliah');
+            $table->string('nilai_huruf');
+            $table->integer('nilai_angka');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('nilai');
+    }
+};
